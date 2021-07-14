4df26fc7c2740f02c260be27a77cef0ddd858bd1
Ncognxto <c.hicks2929@gmail.com>
2021-07-14 15:54:17 -0500

    Updated coding-events

--- coding-events/src/main/java/org/launchcode/codingevents/data/EventData.java
+++ coding-events/src/main/java/org/launchcode/codingevents/data/EventData.java
@@ -1,4 +1,4 @@
-package org.launchcode.codingevents.Data;
+package org.launchcode.codingevents.data;
 
 import org.launchcode.codingevents.models.Event;
 
@@ -6,6 +6,9 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Created by Chris Bay
+ */
 public class EventData {
 
     private static final Map<Integer, Event> events = new HashMap<>();
@@ -25,4 +28,5 @@ public class EventData {
     public static void remove(int id) {
         events.remove(id);
     }
+
 }
